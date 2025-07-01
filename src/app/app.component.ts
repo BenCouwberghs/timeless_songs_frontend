1a511c854138a728e366b9e447e446d7a5139883
Andre Couwberghs <andre.couwberghs@feco.be>
2025-07-01 14:32:58 +0200

    #4 : Just simple show the existing bands as a first demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { environment } from '../environments/environment';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'timeless_songs_frontend';
+
+  private apiUrl = environment.apiBaseUrl;
+  bands: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    console.log('Init');
+    this.http.get<any[]>(`${this.apiUrl}/bands`).subscribe(data => this.bands = data);
+   }
 }
