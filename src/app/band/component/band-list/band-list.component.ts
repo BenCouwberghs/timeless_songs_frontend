bcb6c3d0ce73a13339871080041b536dca6f1fc7
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-07 19:39:19 +0200

    #10: Feat: Generated band-list component.

---
+++ src/app/band/component/band-list/band-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-band-list',
+  imports: [],
+  templateUrl: './band-list.component.html',
+  styleUrl: './band-list.component.scss'
+})
+export class BandListComponent {
+
+}
