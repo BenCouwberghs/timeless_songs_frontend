8bb6ee7a52935d2f5ccd82ddfba86f8d958bf69b
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-07 19:39:32 +0200

    #10: Feat: Generated new-band-form component

---
+++ src/app/band/component/new-band-form/new-band-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-band-form',
+  imports: [],
+  templateUrl: './new-band-form.component.html',
+  styleUrl: './new-band-form.component.scss'
+})
+export class NewBandFormComponent {
+
+}
