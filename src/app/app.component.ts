47c0911ecee1b0d45457e1538a11c0dc55c3cced
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-07 21:10:15 +0200

    #10: Feat: Updated the app component, adding 2 buttons to go to the new components and removing unnecessary code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { environment } from '../environments/environment';
@@ -7,7 +7,7 @@ import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HttpClientModule],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,12 +15,10 @@ export class AppComponent  implements OnInit {
   title = 'timeless_songs_frontend';
 
   private apiUrl = environment.apiBaseUrl;
-  bands: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     console.log('Init');
-    this.http.get<any[]>(`${this.apiUrl}/bands`).subscribe(data => this.bands = data);
    }
 }
