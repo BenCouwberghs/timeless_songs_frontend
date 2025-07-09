2a083aad0cb51611006a6327003e9b85c65257bd
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-09 21:30:38 +0200

    #10: feat: Added properties, a constructor and the methods onSave() to be called by the form and saveBand to send the obtained information to the backend.

--- src/app/band/component/new-band-form/new-band-form.component.ts
+++ src/app/band/component/new-band-form/new-band-form.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-new-band-form',
-  imports: [],
+  imports: [
+  FormsModule,
+    ],
   templateUrl: './new-band-form.component.html',
   styleUrl: './new-band-form.component.scss'
 })
 export class NewBandFormComponent {
+  private baseUrl = 'http://localhost:8080/api';
+  name: string = '';
+  linkWikiPage: string  = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSave() {
+    this.saveBand(this.name, this.linkWikiPage)
+    this.router.navigate(['/band-list']);
+    }
+
+  saveBand(name: string, linkWikiPage: string): Observable<string> {
+    const bandDto = { name, linkWikiPage };
+    return this.http.post<string>(`${this.baseUrl}/bands`, bandDto, {responseType: 'text' as 'json'});
+    }
 }
