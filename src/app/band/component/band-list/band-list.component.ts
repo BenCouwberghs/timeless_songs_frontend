4b1da170418d6f482f989838fa87d2c293a3e9a1
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-09 19:59:16 +0200

    #10: Feat: Added a button the BandLIstComponent that points to the NewBandFormComponent

--- src/app/band/component/band-list/band-list.component.ts
+++ src/app/band/component/band-list/band-list.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-band-list',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterLink],
   templateUrl: './band-list.component.html',
   styleUrl: './band-list.component.scss'
 })
