a4619b51e5a0151f5f221117d68326d36a36cced
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-10 14:43:13 +0200

    #10: feat: changed the url to base upon the one provided via environment.ts. Added a subscribe call whose absence caused the http request to not get to the backend. Can now successfully add bands to our db!

--- src/app/band/component/new-band-form/new-band-form.component.ts
+++ src/app/band/component/new-band-form/new-band-form.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import {FormsModule} from "@angular/forms";
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-new-band-form',
@@ -13,19 +14,21 @@ import {FormsModule} from "@angular/forms";
   styleUrl: './new-band-form.component.scss'
 })
 export class NewBandFormComponent {
-  private baseUrl = 'http://localhost:8080/api';
+  private apiUrl = environment.apiBaseUrl;
   name: string = '';
   linkWikiPage: string  = '';
 
   constructor(private http: HttpClient, private router: Router) {}
 
   onSave() {
-    this.saveBand(this.name, this.linkWikiPage)
-    this.router.navigate(['/band-list']);
+    this.saveBand(this.name, this.linkWikiPage).subscribe({
+      next: () => this.router.navigate(['/band-list']),
+      error: err => console.error('Error:', err)
+      });
     }
 
   saveBand(name: string, linkWikiPage: string): Observable<string> {
     const bandDto = { name, linkWikiPage };
-    return this.http.post<string>(`${this.baseUrl}/bands`, bandDto, {responseType: 'text' as 'json'});
+    return this.http.post<string>(`${this.apiUrl}/bands`, bandDto, {responseType: 'text' as 'json'});
     }
 }
