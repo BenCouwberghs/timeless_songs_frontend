886fe008d42b879419c21d3a8ef2f796666fef14
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-14 18:32:57 +0200

    #12: feat: Generated band information component.

---
+++ src/app/band/component/band-information/band-information.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-band-information',
+  imports: [],
+  templateUrl: './band-information.component.html',
+  styleUrl: './band-information.component.scss'
+})
+export class BandInformationComponent {
+
+}
