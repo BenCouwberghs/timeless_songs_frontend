db044a9030c2c713293edde442ee078683abf737
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-16 21:33:58 +0200

    #12: feat: Refactored BandInformationComponent.

--- src/app/band/component/band-information/band-information.component.ts
+++ src/app/band/component/band-information/band-information.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
 import {FormsModule} from "@angular/forms";
-import { Observable } from 'rxjs';
+import { BandService } from '../../service/band.service';
 
 @Component({
   selector: 'app-band-information',
@@ -13,26 +11,21 @@ import { Observable } from 'rxjs';
   styleUrl: './band-information.component.scss'
 })
 export class BandInformationComponent implements OnInit {
-    private apiUrl = environment.apiBaseUrl;
     band: any;
     showForm = false;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router : Router) {}
+  constructor(private route: ActivatedRoute, private router : Router, private bandService: BandService) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.http.get<any>(`${this.apiUrl}/bands/${id}`).subscribe(res => this.band = res);
+    this.bandService.fetchBand(id).subscribe(res => this.band = res);
     }
 
   onSave() {
-    this.modifyBand(this.band.name, this.band.linkWikiPage, this.band.id).subscribe({
+    this.bandService.modifyBand(this.band.name, this.band.linkWikiPage, this.band.id).subscribe({
           next: () => this.router.navigate(['/band-list']),
           error: err => console.error('Error:', err)
           });
     }
 
-  modifyBand(name: string, linkWikiPage: string, id: number): Observable<string> {
-      const bandDto = { id, name, linkWikiPage };
-      return this.http.patch<string>(`${this.apiUrl}/bands/${id}`, bandDto, {responseType: 'text' as 'json'});
-      }
 }
