2112aaedf1cfeb965cd5761c470a336de7f4a9de
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-22 22:54:17 +0200

    #16: feat: Implemented 2 methods onSearch() and clear() to reset the properties to default values.

--- src/app/band/component/band-list/band-list.component.ts
+++ src/app/band/component/band-list/band-list.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import {FormsModule} from "@angular/forms";
 import { BandService } from '../../service/band.service';
 
 @Component({
   selector: 'app-band-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './band-list.component.html',
   styleUrl: './band-list.component.scss'
 })
 export class BandListComponent implements OnInit {
     bands: any[] = [];
+    keyword: string = '';
+    showClearButton = false;
 
     constructor(private bandService: BandService) {}
 
     ngOnInit() {
       this.bandService.fetchBands().subscribe(data => this.bands = data);
+     }
+
+   onSearch() {
+      this.bandService.search(this.keyword).subscribe(data => this.bands = data);
+     }
+
+   clear() {
+     this.keyword = '';
+     this.showClearButton = false;
+     this.ngOnInit();
      }
 }
