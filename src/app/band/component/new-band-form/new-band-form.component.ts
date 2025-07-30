cff2490c31dc5e0bd3c4f4371a1d1baa53746d2e
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-07-30 23:05:23 +0200

    #29: Feat: Imported MessageService and added the code for a toast message to be displayed on successful save.

--- src/app/band/component/new-band-form/new-band-form.component.ts
+++ src/app/band/component/new-band-form/new-band-form.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { BandService } from '../../service/band.service';
 import { InputTextModule } from 'primeng/inputtext';
 import { FluidModule } from 'primeng/fluid';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-new-band-form',
   imports: [
-  FormsModule, InputTextModule, FluidModule,
+  FormsModule, InputTextModule, FluidModule, ToastModule, ButtonModule
     ],
+  providers: [MessageService],
   templateUrl: './new-band-form.component.html',
   styleUrl: './new-band-form.component.scss'
 })
@@ -17,7 +21,7 @@ export class NewBandFormComponent {
   name: string = '';
   linkWikiPage: string  = '';
 
-  constructor(private router: Router, private bandService: BandService ) {}
+  constructor(private router: Router, private bandService: BandService, private messageService: MessageService ) {}
 
   onSave() {
     if(this.name == '') {
@@ -26,7 +30,13 @@ export class NewBandFormComponent {
       }
 
     this.bandService.saveBand(this.name, this.linkWikiPage).subscribe({
-      next: () => this.router.navigate(['/band-list']),
+      next: () => {
+        this.messageService.add({ severity: 'success', summary: 'success', detail: `Band ${this.name} has been added`,
+          life: 3000});
+          setTimeout(() => {
+            this.router.navigate(['/band-list']);
+          }, 3000);
+        },
       error: err => {
         console.error('Error:', err);
         alert(err.message);
