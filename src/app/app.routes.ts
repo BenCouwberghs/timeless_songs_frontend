b22b0cfd0d680d182a7976fda50e449ca9c02bac
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-08-01 12:09:13 +0200

    #22: Feat: Added cCredits page to routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {BandListComponent} from './band/component/band-list/band-list.component'
 import {NewBandFormComponent} from './band/component/new-band-form/new-band-form.component';
 import {BandInformationComponent} from './band/component/band-information/band-information.component';
 import {HeroComponent} from './pages/hero/hero.component';
+import {CreditsComponent} from './pages/credits/credits.component';
 
 export const routes: Routes = [
 {path: '', component: HeroComponent},
 {path: 'hero', component: HeroComponent},
 {path: 'band-list', component: BandListComponent},
 {path: 'band-form', component: NewBandFormComponent},
-{path: 'band-information/:id', component: BandInformationComponent}
+{path: 'band-information/:id', component: BandInformationComponent},
+{path: 'credits', component: CreditsComponent}
   ];
