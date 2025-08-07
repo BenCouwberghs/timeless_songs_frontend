54969b968eb063c6797468f3c744b6f17a02f319
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-08-07 15:09:38 +0200

    #49: Feat: Updated App component so make the NotificationService work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { ButtonModule } from 'primeng/button';
-
+import { ToastModule } from 'primeng/toast';
 import { environment } from '../environments/environment';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, RouterLink, ButtonModule],
+  imports: [RouterOutlet, HttpClientModule, RouterLink, ButtonModule, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
