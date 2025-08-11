f4525150a871f19b85483a38d39fa382e18b0525
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-08-12 01:27:50 +0200

    #47: Feat: Brought onDelete and deleteEvent from band-information component over. Refactored ngOnInit.

--- src/app/band/component/new-band-form/new-band-form.component.ts
+++ src/app/band/component/new-band-form/new-band-form.component.ts
@@ -7,13 +7,15 @@ import { FluidModule } from 'primeng/fluid';
 import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 import { NotificationService } from '../../../service/notification.service'
+import { ConfirmationService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-new-band-form',
   imports: [
-  FormsModule, InputTextModule, FluidModule, ToastModule, ButtonModule
+  FormsModule, InputTextModule, FluidModule, ToastModule, ButtonModule, ConfirmDialogModule
     ],
-  providers: [],
+  providers: [ConfirmationService],
   templateUrl: './new-band-form.component.html',
   styleUrl: './new-band-form.component.scss'
 })
@@ -24,7 +26,8 @@ export class NewBandFormComponent {
   update = false;
   id: any;
 
-  constructor(private route: ActivatedRoute, private router: Router, private bandService: BandService, private notificationService: NotificationService ) {}
+  constructor(private route: ActivatedRoute, private router: Router, private bandService: BandService,
+    private notificationService: NotificationService, private confirmationService: ConfirmationService) {}
 
 // Have init check if we get an id variable passed, if so execute code to show the update side
 // Otherwise we know we need to show the add band side
@@ -32,26 +35,22 @@ export class NewBandFormComponent {
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
 
-    if(this.id == null) {
-      // Execute add side
-      // Place code to show the appropriate form and buttons
-      } else {
-        // Execute update side
-        this.update = true;
-        this.bandService.fetchBand(this.id).subscribe(res => {
-          this.band = res;
-          this.name = this.band.name;
-          this.linkWikiPage = this.band.linkWikiPage;
-          });
-        }
-        // Place code to show the appropriate form and buttons
+    if(this.id != null) {
+      // Execute update side
+       this.update = true;
+       this.bandService.fetchBand(this.id).subscribe(res => {
+         this.band = res;
+         this.name = this.band.name;
+         this.linkWikiPage = this.band.linkWikiPage;
+       });
     }
+  }
 
   onSave() {
     if(this.name == '') {
       this.notificationService.sendError('Error', 'Band name cannot be empty.');
       return;
-      }
+    }
 
     if(this.update == false) {
     this.bandService.saveBand(this.name, this.linkWikiPage).subscribe({
@@ -78,4 +77,44 @@ export class NewBandFormComponent {
   onCancel() {
     this.router.navigate(['/band-list']);
   }
+
+  onDelete() {
+    this.bandService.deleteBand(this.id).subscribe({
+        next: () => {
+            this.notificationService.sendInfo('Confirmed', `You have deleted the band ${this.band.name}`);
+            this.router.navigate(['/band-list']);
+            },
+        error: err => {
+          console.error('Error:', err);
+          this.notificationService.sendError('Error', err.message);
+          }
+        });
+    }
+
+  deleteEvent(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: `Do you want to delete the band ${this.band.name}?`,
+      header: 'Confirm delete action',
+      icon: 'pi pi-info-circle',
+      rejectLabel: 'Cancel',
+      rejectButtonProps: {
+          label: 'Cancel',
+          severity: 'secondary',
+          outlined: true,
+      },
+      acceptButtonProps: {
+          label: 'Delete',
+          severity: 'danger',
+      },
+
+      accept: () => {
+        this.onDelete();
+      },
+      reject: () => {
+        this.notificationService.sendError('Rejected', 'You have rejected');
+      },
+
+      })
+    }
 }
