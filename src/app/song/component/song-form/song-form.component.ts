8751d0ee45ab77e741544209d70df33259fec1cd
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-09-17 19:11:11 +0200

    #71: Feat: Implemented SelectModule and added ngOnInit to populate Bands array.

--- src/app/song/component/song-form/song-form.component.ts
+++ src/app/song/component/song-form/song-form.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from "@angular/forms";
 import { SongService } from '../../service/song.service';
+import { BandService } from '../../../band/service/band.service';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { FluidModule } from 'primeng/fluid';
@@ -10,11 +11,12 @@ import { ConfirmationService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-song-form',
   imports: [FormsModule, InputTextModule, ButtonModule, FluidModule, ConfirmDialogModule, InputGroupModule,
-    InputGroupAddonModule],
+    InputGroupAddonModule, SelectModule],
   providers: [ConfirmationService],
   templateUrl: './song-form.component.html',
 })
@@ -23,16 +25,23 @@ export class SongFormComponent {
   name: string = '';
   linkWikiPage: string  = '';
   year: number = 0;
-  band: any;
+  selectedBand: any;
+  bands: any[] = [];
   song: any;
   update = false;
   id: any;
 
-  constructor(private router: Router, private songService: SongService, private notificationService: NotificationService,
-    private confirmationService: ConfirmationService) {}
+  constructor(private router: Router, private songService: SongService, private bandService: BandService,
+    private notificationService: NotificationService,private confirmationService: ConfirmationService) {}
+
+  ngOnInit() {
+      this.bandService.fetchBands().subscribe(data => {
+        this.bands = data;
+      })
+  }
 
   onSave() {
-    this.songService.saveSong(this.name, this.band, this.year, this.linkWikiPage).subscribe({
+    this.songService.saveSong(this.name, this.selectedBand, this.year, this.linkWikiPage).subscribe({
       next: () => {
         this.notificationService.sendSuccess('Success', `song ${this.name} has been added`);
         this.gotoSongList();
