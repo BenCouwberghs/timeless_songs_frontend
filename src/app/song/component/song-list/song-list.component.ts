23e86b278950a5a89fdb28a1fd9f7bfe3d7e429b
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-09-18 13:43:48 +0200

    #71: Feat: Added imports of RouterLink, Button and Tooltip modules.

--- src/app/song/component/song-list/song-list.component.ts
+++ src/app/song/component/song-list/song-list.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { SongService } from '../../service/song.service';
 import { ToolbarModule } from 'primeng/toolbar';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-song-list',
   standalone: true,
-  imports: [ToolbarModule],
+  imports: [ToolbarModule, RouterLink, ButtonModule, TooltipModule],
   templateUrl: './song-list.component.html',
 })
 export class SongListComponent implements OnInit {
