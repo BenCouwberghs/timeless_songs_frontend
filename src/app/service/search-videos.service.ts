4b44744f6fb3af944f3694e02dccd941cd5402fe
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-13 02:32:31 +0100

    #119: Feat: Added the missing request params.

--- src/app/service/search-videos.service.ts
+++ src/app/service/search-videos.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
 
@@ -12,6 +12,10 @@ export class SearchVideosService {
   constructor(private http: HttpClient) {}
 
   search(bandName: string, songTitle: string): Observable<any[]> {
-      return this.http.get<any[]>(`${this.apiUrl}/search-videos`);
+    const params = new HttpParams()
+      .set('bandName', bandName)
+      .set('songName', songTitle);
+
+    return this.http.get<any[]>(`${this.apiUrl}/search-videos`, { params });
   }
 }
