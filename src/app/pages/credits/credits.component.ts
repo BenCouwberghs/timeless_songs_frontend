7547da4930f543adaed9c93c1734447d696e416b
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-18 23:02:24 +0100

    #128: Feat: Remove the footer from the credits page.

--- src/app/pages/credits/credits.component.ts
+++ src/app/pages/credits/credits.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { DividerModule } from 'primeng/divider';
 
 
 @Component({
   selector: 'app-credits',
   standalone: true,
-  imports: [DividerModule],
+  imports: [],
   templateUrl: './credits.component.html',
 })
 export class CreditsComponent {
